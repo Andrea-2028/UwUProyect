2c5dc103940eba0ef54964e81ed5eccedc367fd1
Andrea-2028 <tareasandrea2012@gmail.com>
2025-09-21 19:37:37 -0600

    seender roles

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\User;
+use App\Models\Role;
 use Illuminate\Support\Facades\Hash; 
 
 class DatabaseSeeder extends Seeder
@@ -16,6 +17,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        $adminRole = Role::firstOrCreate(['name' => 'admin']);
+        $visitorRole = Role::firstOrCreate(['name' => 'visitor']);
+        
         $user = User::create([    
             'first_name' => 'Andrea',    
             'last_name'  => 'GuelDesarrollador',    
